32ff4fb0f470e3e4f2523d961d3d260e96464033
Lucas Rufo <rifelu25@outlook.com>
2021-10-09 14:13:18 -0300

    feat: add toast and fix api call

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,9 +1,21 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL as string ?? '';
 
 const api = axios.create({
   baseURL: API_URL,
+});
+
+api.interceptors.response.use(function (response) {
+  return response;
+}, function (error: AxiosError) {
+  
+  console.log(error)
+  if(error.code === '401') {
+    //sign out
+  }
+
+  return Promise.reject(error);
 });
 
 export default api;
--- client/src/components/CreateUser.tsx
+++ client/src/components/CreateUser.tsx
@@ -1,31 +1,64 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useState } from "react";
-import { AxiosError } from "axios";
+import { toast } from 'react-toastify';
+import { User } from "../types/User";
 import api from "../api/api";
+import LoadingSpinner from "./LoadingSpinner";
 
 export default function CreateUser() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
+  const [loading, setLoading] = useState(false);
+  let history = useHistory();
+
+  async function createUser(user: User) {
+    try {
+      setLoading(true);
+
+      await api.post('user', user);
+
+      toast.success('Usuário criado com sucesso', {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        progress: undefined,
+      });
+
+      history.push('/');
+    }
+    catch (error: any) {
+      setLoading(false);
+      if (error.response) {
+        toast.error('Erro ao criar usuário', {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          progress: undefined,
+        });
+      }
+      else {
+        console.log(error)
+      }
+    }
+  }
+
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    const user = {
+    const user: User = {
       email,
       password
     };
 
-    try {
-      const response = await api.post('user', user);
-    }
-    catch (error: any) {
-      if(error.response) {
-        console.log(error.response)
-      } 
-      else {
-        console.log(error.response)
-      }
-    }
+    await createUser(user);
+  }
+
+  if (loading) {
+    return (
+      <LoadingSpinner />
+    )
   }
 
   return (
@@ -33,23 +66,25 @@ export default function CreateUser() {
       <h1 className="text-xl text-gray-800 text-center">Criar Conta</h1>
       <div className="flex flex-col mt-3">
         <label htmlFor="email">Email</label>
-        <input 
-          onChange={(e) => setEmail(e.target.value)} 
-          value={email} 
-          className="rounded-sm focus:ring-1 focus:ring-indigo-200" 
-          type="text" 
-          name="email" 
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          className="rounded-sm focus:ring-1 focus:ring-indigo-200"
+          type="text"
+          name="email"
           id="email" />
+        <small>{ }</small>
       </div>
       <div className="flex flex-col mt-3">
         <label htmlFor="password">Senha</label>
-        <input 
-          onChange={(e) => setPassword(e.target.value)} 
-          value={password} 
-          className="rounded-sm focus:ring-1 focus:ring-indigo-200" 
-          type="password" 
-          name="password" 
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          className="rounded-sm focus:ring-1 focus:ring-indigo-200"
+          type="password"
+          name="password"
           id="password" />
+        <small>{ }</small>
       </div>
       <div className="flex justify-center mt-5">
         <Link to="/">
